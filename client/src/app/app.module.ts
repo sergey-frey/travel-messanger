0511e3ef11817c18265b8300654784fc7588ec3c
3str-webdev <3str.webdev@gmail.com>
2023-03-28 21:03:48 +0300

    feat: add taiga-ui lib
    UI Kit for Angular

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,3 +1,6 @@
+import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
+import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER } from "@taiga-ui/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -7,8 +10,8 @@ import { StoreModule } from '@ngrx/store';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule, StoreModule.forRoot({}, {})],
-	providers: [],
+	imports: [BrowserModule, AppRoutingModule, StoreModule.forRoot({}, {}), BrowserAnimationsModule, TuiRootModule, TuiDialogModule, TuiAlertModule],
+	providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}],
 	bootstrap: [AppComponent],
 })
 export class AppModule {}
