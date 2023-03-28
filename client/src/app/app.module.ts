afb9b382e864c7b59cef2bb96b97f03286f57caa
3str-webdev <3str.webdev@gmail.com>
2023-03-28 20:59:29 +0300

    feat: add NgRx lib
    Lib for state management

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule],
+	imports: [BrowserModule, AppRoutingModule, StoreModule.forRoot({}, {})],
 	providers: [],
 	bootstrap: [AppComponent],
 })
