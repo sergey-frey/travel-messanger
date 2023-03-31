9ca0cd749ec478f7feeb847992f0988a4f314a79
3str-webdev <3str.webdev@gmail.com>
2023-04-01 00:37:00 +0300

    feat: add user page route

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
+import { UserPageComponent } from './pages/user-page/user-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'profile', component: UserPageComponent, title: 'Профиль' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
