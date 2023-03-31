8feaab66ec1492ba7a06d005994906c8a3c9990d
3str-webdev <3str.webdev@gmail.com>
2023-03-31 23:16:23 +0300

    feat: add folder structure and user-page module

---
+++ client/src/app/pages/user-page/user-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-page',
+  templateUrl: './user-page.component.html',
+  styleUrls: ['./user-page.component.less']
+})
+export class UserPageComponent {
+
+}
