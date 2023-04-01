e7b55da1d4bb269555a8e93e6d1cdc614a83c427
3str-webdev <3str.webdev@gmail.com>
2023-04-01 12:01:03 +0300

    feat: add user avatar module in SHARED

---
+++ client/src/app/shared/modules/user-avatar/user-avatar.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-user-avatar',
+  templateUrl: './user-avatar.component.html',
+  styleUrls: ['./user-avatar.component.less']
+})
+export class UserAvatarComponent {
+  @Input() public src!: string;
+}
