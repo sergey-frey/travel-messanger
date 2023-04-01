433253048800ae3905de675ff816d9c0bb0b3d3f
3str-webdev <3str.webdev@gmail.com>
2023-04-01 11:58:39 +0300

    feat: add universal secftion component for user page

---
+++ client/src/app/pages/user-page/components/user-page-section/user-page-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-page-section',
+  templateUrl: './user-page-section.component.html',
+  styleUrls: ['./user-page-section.component.less']
+})
+export class UserPageSectionComponent {
+
+}
