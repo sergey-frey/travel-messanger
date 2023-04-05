acd1e434a11185e946b9a4a0a09fd2f80a9fe652
3str-webdev <3str.webdev@gmail.com>
2023-04-05 12:24:19 +0300

    feat: create layout empty component

---
+++ client/src/app/layout/layout.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LayoutComponent } from './layout.component';
+
+@NgModule({
+  declarations: [LayoutComponent],
+  imports: [CommonModule],
+  exports: [LayoutComponent]
+})
+export class LayoutModule {}
