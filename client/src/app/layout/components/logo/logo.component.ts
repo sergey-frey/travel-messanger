5030f6d357db6300f291b234f5c2743da7517f84
3str-webdev <3str.webdev@gmail.com>
2023-04-11 17:58:12 +0300

    feat: add search component, notification and theme buttons, logo, avatar

---
+++ client/src/app/layout/components/logo/logo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logo',
+  templateUrl: './logo.component.html',
+  styleUrls: ['./logo.component.less']
+})
+export class LogoComponent {
+
+}
--- client/src/app/layout/layout.module.ts
+++ client/src/app/layout/layout.module.ts
@@ -1,10 +1,35 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import {
+  TuiButtonModule, TuiTextfieldControllerModule
+} from '@taiga-ui/core';
+import { TuiBadgedContentModule, TuiInputModule } from '@taiga-ui/kit';
+import { AvatarComponent } from './components/avatar/avatar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { SearchInputComponent } from './components/search-input/search-input.component';
+import { ThemeToggleButtonComponent } from './components/theme-toggle-button/theme-toggle-button.component';
 import { LayoutComponent } from './layout.component';
+import { NotificationButtonComponent } from './components/notification-button/notification-button.component';
 
 @NgModule({
-  declarations: [LayoutComponent],
-  imports: [CommonModule],
+  declarations: [
+    LayoutComponent,
+    HeaderComponent,
+    ThemeToggleButtonComponent,
+    SearchInputComponent,
+    LogoComponent,
+    AvatarComponent,
+    NotificationButtonComponent,
+
+  ],
+  imports: [
+    CommonModule,
+    TuiInputModule,
+    TuiButtonModule,
+    TuiTextfieldControllerModule,
+    TuiBadgedContentModule
+  ],
   exports: [LayoutComponent]
 })
 export class LayoutModule {}
