bd3e5e507ad158b09319bcecb046885b23624522
DmiKul <mr.crocodile567@gmail.com>
2023-04-14 20:48:18 +0300

    feat: add post images interface, add icons of arrow buttons, change post control bar padding

---
+++ client/src/app/shared/modules/post/components/post-images/post-images.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-images',
+  templateUrl: './post-images.component.html',
+  styleUrls: ['./post-images.component.less']
+})
+export class PostImagesComponent {
+
+}
--- client/src/app/shared/modules/post/post.module.ts
+++ client/src/app/shared/modules/post/post.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TuiButtonModule } from '@taiga-ui/core';
 import { PostControlBarComponent } from './components/post-control-bar/post-control-bar.component';
+import { PostImagesComponent } from './components/post-images/post-images.component';
 
 @NgModule({
-  declarations: [PostComponent, PostControlBarComponent],
+  declarations: [PostComponent, PostControlBarComponent, PostImagesComponent],
   imports: [CommonModule, TuiButtonModule],
   exports: [PostComponent]
 })
