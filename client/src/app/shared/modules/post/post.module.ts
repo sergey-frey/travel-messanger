6655a7a683d39ca45b585370e09c46db095db536
DmiKul <mr.crocodile567@gmail.com>
2023-04-16 22:21:44 +0300

    feat: add tui-carousel component, change styles for it

--- client/src/app/shared/modules/post/post.module.ts
+++ client/src/app/shared/modules/post/post.module.ts
@@ -4,10 +4,14 @@ import { CommonModule } from '@angular/common';
 import { TuiButtonModule } from '@taiga-ui/core';
 import { PostControlBarComponent } from './components/post-control-bar/post-control-bar.component';
 import { PostImagesComponent } from './components/post-images/post-images.component';
+import { TuiSvgModule} from '@taiga-ui/core';
+import {TuiCarouselModule, TuiPaginationModule} from '@taiga-ui/kit';
+
+
 
 @NgModule({
   declarations: [PostComponent, PostControlBarComponent, PostImagesComponent],
-  imports: [CommonModule, TuiButtonModule],
+  imports: [CommonModule, TuiButtonModule, TuiSvgModule, TuiCarouselModule, TuiPaginationModule],
   exports: [PostComponent]
 })
 export class PostModule {}
