eb2590ff4d3f422287cea1cc7d3730c05a744707
DmiKul <mr.crocodile567@gmail.com>
2023-04-27 20:43:14 +0300

    feat: replace title with 2 others, add static sizes for friens-list block, add count of friends, online friends, showing only first 4 ones

--- client/src/app/pages/user-page/components/friends-section/friends-section.component.ts
+++ client/src/app/pages/user-page/components/friends-section/friends-section.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./friends-section.component.less']
 })
 export class FriendsSectionComponent {
-
+  friends: object[] = [{},{}, {}, {}, {}, {}, {}, {}, {}, {}, {},{}, {}, {}, {}, {}, {}, {}, {}, {}]
+  onlineFriends: object[] = [{},{}, {}, {}, {}, {}, {}, {}, {}, {}]
+  friendsCount: number = this.friends.length
+  onlineFriendsCount: number = this.onlineFriends.length
+  firstFriends: object[] = this.friends.slice(0, 4)
+  firstOnlineFriends: object[] = this.onlineFriends.slice(0, 4)
 }
