5b741b489fe4cd8e583a3aa4e3f06adbbf4dde97
DmiKul <mr.crocodile567@gmail.com>
2023-05-03 01:26:23 +0300

    fix: change path for friendModel

--- client/src/app/pages/user-page/components/friends-section/friends-section.component.ts
+++ client/src/app/pages/user-page/components/friends-section/friends-section.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { Friend } from '@customTypes/models/friendModel';
+import { Friend } from '../../../../types/models/friendModel';
 
 @Component({
   selector: 'app-friends-section',
